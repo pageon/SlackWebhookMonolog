7eff327b7b59a5044031710a8bfef4760b8e2cd6
Jelmer Prins <jelmer@sumocoders.be>
2016-02-13 14:22:30 +0100

    Slim down the Attachment class
    We removed the builder and combined the attachment and the builder.
    All it needs to do is to set the things you need and get the data in array format out of it.

--- src/Slack/Attachment/Attachment.php
+++ src/Slack/Attachment/Attachment.php
@@ -15,93 +15,19 @@ use Pageon\SlackWebhookMonolog\General\Url;
 final class Attachment implements JsonSerializable
 {
     /**
-     * A plain-text summary of the attachment.
-     * This text will be used in clients that don't show formatted text (eg. IRC, mobile notifications).
-     * It should not contain any markup.
-     *
-     * @var string
-     */
-    private $fallback;
-
-    /**
-     * This is the main text in a message attachment, and can contain standard message markup.
-     * The content will automatically collapse if it contains 700+ characters or 5+ linebreaks,
-     * and will display a "Show more..." link to expand the content.
-     *
-     * @var string|null
-     */
-    private $text = null;
-
-    /**
-     * This value is used to color the border along the left side of the message attachment.
-     *
-     * @var Colour|null
-     */
-    private $colour = null;
-
-    /**
-     * This is optional text that appears above the message attachment block.
-     *
-     * @var string|null
-     */
-    private $pretext = null;
-
-    /**
-     * The author parameters will display a small section at the top of a message attachment.
-     *
-     * @var Author|null
-     */
-    private $author = null;
-
-    /**
-     * The title is displayed as larger, bold text near the top of a message attachment.
-     * By passing a valid URL in the link parameter (optional), the title will be hyperlinked.
-     *
-     * @var Title|null
-     */
-    private $title = null;
-
-    /**
-     * Will be displayed in a table inside the message attachment.
-     *
-     * @var Field[]
-     */
-    private $fields = [];
-
-    /**
-     * A valid URL to an image file that will be displayed inside a message attachment.
-     * We currently support the following formats: GIF, JPEG, PNG, and BMP.
-     * Large images will be resized to a maximum width of 400px or a maximum height of 500px,
-     * while still maintaining the original aspect ratio.
-     *
-     * @var Url|null
-     */
-    private $image = null;
-
-    /**
-     * A valid URL to an image file that will be displayed as a thumbnail on the right side of a message attachment.
-     * We currently support the following formats: GIF, JPEG, PNG, and BMP.
-     * The thumbnail's longest dimension will be scaled down to 75px while maintaining the aspect ratio of the image.
-     * The filesize of the image must also be less than 500 KB.
-     *
-     * @var Url|null
-     */
-    private $thumbnail = null;
-
-    /**
-     * By default bot message text will be formatted, but attachments are not.
-     * Attachments need to enable the markdown manually.
+     * The attachment itself.
+     * We enable markdown in everything, why wouldn't we?
      *
      * @var array
      */
-    private $markdownIn = [];
+    private $attachment = ['mrkdwn_in' => ["pretext", "text", "fields"]];
 
     /**
      * Attachment constructor.
      * I opted for setters since there are so many optional parameters.
      * A new instance would look be messy if you only needed the last optional parameter.
      *
-     * @param string $fallback
+     * @param string $fallback A plain-text summary of the attachment.
      */
     public function __construct($fallback)
     {
@@ -109,297 +35,151 @@ final class Attachment implements JsonSerializable
             throw new \InvalidArgumentException('The fallback is required');
         }
 
-        $this->fallback = $fallback;
+        $this->attachment['fallback'] = $fallback;
     }
 
     /**
-     * @return string
-     */
-    public function getFallback()
-    {
-        return $this->fallback;
-    }
-
-    /**
+     * This is the main text in a message attachment.
+     *
      * @param string $text
      *
      * @return self
      */
     public function setText($text)
     {
-        $this->text = $text;
+        $this->attachment['text'] = $text;
 
         return $this;
     }
 
     /**
-     * @return bool
-     */
-    public function hasText()
-    {
-        return $this->text !== null;
-    }
-
-    /**
-     * @return string|null
-     */
-    public function getText()
-    {
-        return $this->text;
-    }
-
-    /**
+     * This value is used to color the border along the left side of the message attachment.
+     *
      * @param Colour $colour
      *
      * @return self
      */
     public function setColour(Colour $colour)
     {
-        $this->colour = $colour;
+        $this->attachment['color'] = $colour;
 
         return $this;
     }
 
     /**
-     * @return bool
-     */
-    public function hasColour()
-    {
-        return $this->colour !== null;
-    }
-
-    /**
-     * @return Colour|null
-     */
-    public function getColour()
-    {
-        return $this->colour;
-    }
-
-    /**
+     * This is optional text that appears above the message attachment block.
+     *
      * @param string $pretext
      *
      * @return self
      */
     public function setPretext($pretext)
     {
-        $this->pretext = $pretext;
+        $this->attachment['pretext'] = $pretext;
 
         return $this;
     }
 
     /**
-     * @return bool
-     */
-    public function hasPretext()
-    {
-        return $this->pretext !== null;
-    }
-
-    /**
-     * @return string|null
-     */
-    public function getPretext()
-    {
-        return $this->pretext;
-    }
-
-    /**
+     * The author parameters will display a small section at the top of a message attachment.
+     *
      * @param Author $author
      *
      * @return self
      */
     public function setAuthor(Author $author)
     {
-        $this->author = $author;
+        $this->attachment['author_name'] = $author;
+
+        if ($author->hasIcon()) {
+            $this->attachment['author_icon'] = $author->getIcon();
+        }
+
+        if ($author->hasLink()) {
+            $this->attachment['author_link'] = $author->getLink();
+        }
 
         return $this;
     }
 
     /**
-     * @return bool
-     */
-    public function hasAuthor()
-    {
-        return $this->author !== null;
-    }
-
-    /**
-     * @return Author|null
-     */
-    public function getAuthor()
-    {
-        return $this->author;
-    }
-
-    /**
+     * The title is displayed as larger, bold text near the top of a message attachment.
+     *
      * @param Title $title
      *
      * @return self
      */
     public function setTitle(Title $title)
     {
-        $this->title = $title;
+        $this->attachment['title'] = $title;
+
+        if ($title->hasLink()) {
+            $this->attachment['title_link'] = $title->getLink();
+        }
 
         return $this;
     }
 
     /**
-     * @return bool
-     */
-    public function hasTitle()
-    {
-        return $this->title !== null;
-    }
-
-    /**
-     * @return Title|null
-     */
-    public function getTitle()
-    {
-        return $this->title;
-    }
-
-    /**
+     * Will be displayed in a table inside the message attachment.
+     *
      * @param Field $field
+     *
+     * @return self
      */
     public function addField(Field $field)
     {
-        $this->fields[] = $field;
+        if (!isset($this->attachment['fields'])) {
+            $this->attachment['fields'] = [];
+        }
+
+        $this->attachment['fields'][] = $field;
+
+        return $this;
     }
 
     /**
+     * Add multiple fields in 1 call.
+     *
      * @param Field[] $fields
+     *
+     * @return self
      */
     public function addFields(array $fields)
     {
         foreach ($fields as $field) {
             $this->addField($field);
         }
+
+        return $this;
     }
 
     /**
-     * @return bool
-     */
-    public function hasFields()
-    {
-        return !empty($this->fields);
-    }
-
-    /**
-     * @return Field[]
-     */
-    public function getFields()
-    {
-        return $this->fields;
-    }
-
-    /**
+     * A valid URL to an image file that will be displayed inside a message attachment.
+     *
      * @param Url $image
      *
      * @return self
      */
     public function setImage(Url $image)
     {
-        $this->image = $image;
+        $this->attachment['image_url'] = $image;
 
         return $this;
     }
 
     /**
-     * @return bool
-     */
-    public function hasImage()
-    {
-        return $this->image !== null;
-    }
-
-    /**
-     * @return Url|null
-     */
-    public function getImage()
-    {
-        return $this->image;
-    }
-
-    /**
+     * A valid URL to an image file that will be displayed as a thumbnail on the right side of a message attachment.
+     *
      * @param Url $thumbnail
      *
      * @return self
      */
     public function setThumbnail(Url $thumbnail)
     {
-        $this->thumbnail = $thumbnail;
+        $this->attachment['thumb_url'] = $thumbnail;
 
         return $this;
-    }
-
-    /**
-     * @return bool
-     */
-    public function hasThumbnail()
-    {
-        return $this->thumbnail !== null;
-    }
-
-    /**
-     * @return Url|null
-     */
-    public function getThumbnail()
-    {
-        return $this->thumbnail;
-    }
-
-    /**
-     * Enable markdown in the pretext.
-     *
-     * @return $this
-     */
-    public function enableMarkdownForPretext()
-    {
-        $this->markdownIn[] = 'pretext';
-
-        return $this;
-    }
-
-    /**
-     * Enable markdown in the text.
-     *
-     * @return $this
-     */
-    public function enableMarkdownForText()
-    {
-        $this->markdownIn[] = 'text';
-
-        return $this;
-    }
-
-    /**
-     * Enable markdown for the value in the fields.
-     *
-     * @return $this
-     */
-    public function enableMarkdownForFieldValues()
-    {
-        $this->markdownIn[] = 'fields';
-
-        return $this;
-    }
-
-    /**
-     * @return bool
-     */
-    public function hasMarkdownSettings()
-    {
-        return !empty($this->markdownIn);
-    }
-
-    /**
-     * @return array
-     */
-    public function getMarkdownSettings()
-    {
-        return $this->markdownIn;
     }
 
     /**
@@ -417,6 +197,6 @@ final class Attachment implements JsonSerializable
      */
     public function get()
     {
-        return (new Builder($this))->build();
+        return $this->attachment;
     }
 }
--- tests/Unit/Slack/Attachment/AttachmentTest.php
+++ tests/Unit/Slack/Attachment/AttachmentTest.php
@@ -24,7 +24,6 @@ class AttachmentTest extends PHPUnit_Framework_TestCase
     {
         $fallback = 'I\'ll notify you';
         $attachment = new Attachment($fallback);
-        $this->assertEquals($fallback, $attachment->getFallback());
 
         $this->assertTrue(isset($attachment->get()['fallback']));
         $this->assertEquals($fallback, $attachment->get()['fallback']);
@@ -39,14 +38,10 @@ class AttachmentTest extends PHPUnit_Framework_TestCase
     {
         $attachment = $this->getAttachment();
 
-        $this->assertNull($attachment->getText());
-        $this->assertFalse($attachment->hasText());
         $this->assertFalse(isset($attachment->get()['text']));
 
         $text = 'This is a dummy text';
         $attachment->setText($text);
-        $this->assertTrue($attachment->hasText());
-        $this->assertEquals($text, $attachment->getText());
 
         $this->assertTrue(isset($attachment->get()['text']));
         $this->assertEquals($text, $attachment->get()['text']);
@@ -58,14 +53,10 @@ class AttachmentTest extends PHPUnit_Framework_TestCase
     {
         $attachment = $this->getAttachment();
 
-        $this->assertNull($attachment->getPretext());
-        $this->assertFalse($attachment->hasPretext());
         $this->assertFalse(isset($attachment->get()['pretext']));
 
         $pretext = 'This is a dummy pretext';
         $attachment->setPretext($pretext);
-        $this->assertTrue($attachment->hasPretext());
-        $this->assertEquals($pretext, $attachment->getPretext());
 
         $this->assertTrue(isset($attachment->get()['pretext']));
         $this->assertEquals($pretext, $attachment->get()['pretext']);
@@ -77,14 +68,10 @@ class AttachmentTest extends PHPUnit_Framework_TestCase
     {
         $attachment = $this->getAttachment();
 
-        $this->assertNull($attachment->getColour());
-        $this->assertFalse($attachment->hasColour());
         $this->assertFalse(isset($attachment->get()['color']));
 
         $colour = new Colour(Colour::COLOUR_GOOD);
         $attachment->setColour($colour);
-        $this->assertTrue($attachment->hasColour());
-        $this->assertEquals($colour, $attachment->getColour());
 
         $this->assertTrue(isset($attachment->get()['color']));
         $this->assertEquals($colour, $attachment->get()['color']);
@@ -96,14 +83,10 @@ class AttachmentTest extends PHPUnit_Framework_TestCase
     {
         $attachment = $this->getAttachment();
 
-        $this->assertNull($attachment->getImage());
-        $this->assertFalse($attachment->hasImage());
         $this->assertFalse(isset($attachment->get()['image_url']));
 
         $image = new Url('http://pageon.be/logo.png');
         $attachment->setImage($image);
-        $this->assertTrue($attachment->hasImage());
-        $this->assertEquals($image, $attachment->getImage());
 
         $this->assertTrue(isset($attachment->get()['image_url']));
         $this->assertEquals($image, $attachment->get()['image_url']);
@@ -115,14 +98,10 @@ class AttachmentTest extends PHPUnit_Framework_TestCase
     {
         $attachment = $this->getAttachment();
 
-        $this->assertNull($attachment->getThumbnail());
-        $this->assertFalse($attachment->hasThumbnail());
         $this->assertFalse(isset($attachment->get()['thumb_url']));
 
         $thumbnail = new Url('http://pageon.be/logo.png');
         $attachment->setThumbnail($thumbnail);
-        $this->assertTrue($attachment->hasThumbnail());
-        $this->assertEquals($thumbnail, $attachment->getThumbnail());
 
         $this->assertTrue(isset($attachment->get()['thumb_url']));
         $this->assertEquals($thumbnail, $attachment->get()['thumb_url']);
@@ -130,41 +109,14 @@ class AttachmentTest extends PHPUnit_Framework_TestCase
         $this->assertContains('"thumb_url":' . json_encode($thumbnail), json_encode($attachment));
     }
 
-    public function testMarkdownSettings()
-    {
-        $attachment = $this->getAttachment();
-
-        $this->assertFalse(isset($attachment->get()['mrkdwn_in']));
-        $this->assertNotContains('mrkdwn_in', json_encode($attachment));
-
-        $attachment->enableMarkdownForFieldValues();
-        $this->assertTrue(isset($attachment->get()['mrkdwn_in']));
-        $this->assertTrue(in_array('fields', $attachment->get()['mrkdwn_in']));
-        $this->assertContains('mrkdwn_in', json_encode($attachment));
-        $this->assertContains('"fields"', json_encode($attachment));
-
-        $this->assertNotContains('text', json_encode($attachment));
-        $attachment->enableMarkdownForText();
-        $this->assertContains('"text"', json_encode($attachment));
-
-        $this->assertNotContains('pretext', json_encode($attachment));
-        $attachment->enableMarkdownForPretext();
-        $this->assertContains('"pretext"', json_encode($attachment));
-    }
-
     public function testAuthor()
     {
         $attachment = $this->getAttachment();
 
-        $this->assertNull($attachment->getAuthor());
-        $this->assertFalse($attachment->hasAuthor());
         $this->assertFalse(isset($attachment->get()['author_url']));
 
         $baseAuthor = new Author('jelmer');
         $attachment->setAuthor($baseAuthor);
-        $this->assertTrue($attachment->hasAuthor());
-        $this->assertEquals($baseAuthor, $attachment->getAuthor());
-
         $this->assertTrue(isset($attachment->get()['author_name']));
         $this->assertEquals($baseAuthor, $attachment->get()['author_name']);
 
@@ -188,14 +140,10 @@ class AttachmentTest extends PHPUnit_Framework_TestCase
     {
         $attachment = $this->getAttachment();
 
-        $this->assertNull($attachment->getTitle());
-        $this->assertFalse($attachment->hasTitle());
         $this->assertFalse(isset($attachment->get()['title_url']));
 
         $baseTitle = new Title('jelmer wrote this');
         $attachment->setTitle($baseTitle);
-        $this->assertTrue($attachment->hasTitle());
-        $this->assertEquals($baseTitle, $attachment->getTitle());
 
         $this->assertTrue(isset($attachment->get()['title']));
         $this->assertEquals($baseTitle, $attachment->get()['title']);
@@ -215,25 +163,14 @@ class AttachmentTest extends PHPUnit_Framework_TestCase
     {
         $attachment = $this->getAttachment();
 
-        $this->assertEmpty($attachment->getFields());
-        $this->assertFalse($attachment->hasFields());
         $this->assertFalse(isset($attachment->get()['fields']));
 
         $field = new Field('title', 'Jelmer wrote it');
         $attachment->addField($field);
-        $this->assertTrue($attachment->hasFields());
-        $this->assertCount(1, $attachment->getFields());
-        $attachment->addField($field);
-        $this->assertCount(2, $attachment->getFields());
-
         $attachmentArray = $attachment->get();
         $this->assertTrue(isset($attachmentArray['fields']));
         $this->assertEquals($field, $attachmentArray['fields'][0]);
-
         $this->assertContains('"fields":', json_encode($attachment));
         $this->assertContains(json_encode($field), json_encode($attachment));
-
-        $attachment->addFields([$field, $field]);
-        $this->assertCount(4, $attachment->getFields());
     }
 }
